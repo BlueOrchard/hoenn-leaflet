57b99815ec6b690fe649a3eb8ca58b694f9ef74d
Joseph Richey <josephrichey@me.com>
2021-03-24 21:03:04 -0400

    Added animation for area

--- src/components/sidebar/Area.js
+++ src/components/sidebar/Area.js
@@ -1,4 +1,6 @@
+import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useState } from "react";
+import { pageSlide, pageTransition, popIn } from "../../util/animations";
 import { fetchIDFromURL } from "../../util/fetchIDFromURL";
 import { removeDashes } from "../../util/removeDashes";
 import PokemonImage from "../pokemon/PokemonImage";
@@ -17,28 +19,36 @@ function Area(props){
     }, [areaData])
 
     return(
-        <div className="area">
-            <span className="area-name-small">Encounter Area</span>
-            <h2 className="area-name">{areaName}</h2>
+        <AnimatePresence>
+            <motion.div 
+                initial="initial"
+                animate="in"
+                exit="out"
+                variants={popIn}
+                transition={pageTransition}
+                className="area">
+                    <span className="area-name-small">Encounter Area</span>
+                    <h2 className="area-name">{areaName}</h2>
 
-            {areaData.pokemon_encounters.map((pokemon_encounter) => (
-                <div className="single-pokemon">
-                    <div className="image">
-                        <PokemonImage ID={fetchIDFromURL(pokemon_encounter.pokemon.url)} />
-                    </div>
-                    <div className="information">
-                        <h3 className="pokemon-name">{pokemon_encounter.pokemon.name}</h3>
-                        {pokemon_encounter.version_details[0].encounter_details.map((encounter_detail) =>(
-                            <div className="encounter-stats">
-                                <p className="method">Method: <span>{removeDashes(encounter_detail.method.name)}</span></p>
-                                <p className="chance">Chance: {encounter_detail.chance}%</p>
-                                <p className="level">Level: {encounter_detail.min_level} - {encounter_detail.max_level}</p>
+                    {areaData.pokemon_encounters.map((pokemon_encounter) => (
+                        <div className="single-pokemon">
+                            <div className="image">
+                                <PokemonImage ID={fetchIDFromURL(pokemon_encounter.pokemon.url)} />
                             </div>
-                        ))}
-                    </div>
-                </div>
-            ))}
-        </div>
+                            <div className="information">
+                                <h3 className="pokemon-name">{pokemon_encounter.pokemon.name}</h3>
+                                {pokemon_encounter.version_details[0].encounter_details.map((encounter_detail) =>(
+                                    <div className="encounter-stats">
+                                        <p className="method">Method: <span>{removeDashes(encounter_detail.method.name)}</span></p>
+                                        <p className="chance">Chance: {encounter_detail.chance}%</p>
+                                        <p className="level">Level: {encounter_detail.min_level} - {encounter_detail.max_level}</p>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
+            </motion.div>
+        </AnimatePresence>
     )
 }
 
